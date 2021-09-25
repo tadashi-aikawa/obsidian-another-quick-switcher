d3243fc231b66d9058d99bd372c8df70da58860d
tadashi-aikawa <syou.maman@gmail.com>
2021-09-25 23:13:43 +0900

    Refactoring

---
+++ src/commands/index.ts
@@ -0,0 +1,45 @@
+import { Mode, SmartSearchModal } from "../ui/SmartSearchModal";
+import { App, Command } from "obsidian";
+
+export function showSearchDialog(app: App, mode: Mode) {
+  const modal = new SmartSearchModal(app, mode);
+  modal.open();
+}
+
+export function createCommands(app: App): Command[] {
+  return [
+    {
+      id: "normal-search",
+      name: "Normal search",
+      hotkeys: [{ modifiers: ["Ctrl", "Shift"], key: "p" }],
+      checkCallback: (checking: boolean) => {
+        if (!checking) {
+          showSearchDialog(app, "normal");
+        }
+        return true;
+      },
+    },
+    {
+      id: "recent-search",
+      name: "Recent search",
+      hotkeys: [{ modifiers: ["Ctrl", "Shift"], key: "e" }],
+      checkCallback: (checking: boolean) => {
+        if (!checking) {
+          showSearchDialog(app, "recent");
+        }
+        return true;
+      },
+    },
+    {
+      id: "backlink-search",
+      name: "Backlink search",
+      hotkeys: [{ modifiers: ["Ctrl", "Shift"], key: "h" }],
+      checkCallback: (checking: boolean) => {
+        if (!checking) {
+          showSearchDialog(app, "backlink");
+        }
+        return true;
+      },
+    },
+  ];
+}
--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,10 @@
 import { Plugin } from "obsidian";
-import { Mode, SmartSearchModal } from "./ui/SmartSearchModal";
+import { createCommands } from "./commands";
 
 export default class SmartSearch extends Plugin {
   async onload() {
     console.log("loading plugin");
 
-    this.addCommand({
-      id: "normal-search",
-      name: "Normal search",
-      hotkeys: [{ modifiers: ["Ctrl", "Shift"], key: "p" }],
-      checkCallback: (checking: boolean) => {
-        if (!checking) {
-          this.showSearchDialog("normal");
-        }
-        return true;
-      },
-    });
-
-    this.addCommand({
-      id: "recent-search",
-      name: "Recent search",
-      hotkeys: [{ modifiers: ["Ctrl", "Shift"], key: "e" }],
-      checkCallback: (checking: boolean) => {
-        if (!checking) {
-          this.showSearchDialog("recent");
-        }
-        return true;
-      },
-    });
-
-    this.addCommand({
-      id: "backlink-search",
-      name: "Backlink search",
-      hotkeys: [{ modifiers: ["Ctrl", "Shift"], key: "h" }],
-      checkCallback: (checking: boolean) => {
-        if (!checking) {
-          this.showSearchDialog("backlink");
-        }
-        return true;
-      },
-    });
-  }
-
-  showSearchDialog(mode: Mode) {
-    const modal = new SmartSearchModal(this.app, mode);
-    modal.open();
+    createCommands(this.app).forEach(this.addCommand);
   }
 }
